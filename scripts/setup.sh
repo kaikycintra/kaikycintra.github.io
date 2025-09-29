b5d12f33fc1374dd29112a2fc77817a84670390b
kaiky <kaikycintra@gmail.com>
2025-09-29 14:42:53 -0300

    ops: adds live server setup via bash script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,4 +3,11 @@ echo 'Configuring pre-push hook...'
 cp scripts/pre-push .git/hooks/pre-push
 chmod ugo+x .git/hooks/pre-push
 echo 'pre-push hook configured!'
+
+echo 'Configuring Live Server...'
+mkdir -p .vscode
+touch .vscode/settings.json
+echo '{
+  "liveServer.settings.file": "index.html"
+}' > .vscode/settings.json
 echo 'Setup complete!'
